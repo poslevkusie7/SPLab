df8dcf43319878bb3b9ba8bde8d3bc01c57c9f8e
poslevkusie <poslevkusie@MacBook-Pro-Poslevkusie.local>
2022-11-10 12:15:16 +0200

    upd splab2

--- SPlab2/sl2_2b.c
+++ SPlab2/sl2_2b.c
@@ -4,6 +4,8 @@
 
 #define BUFFSIZE 10
 
+char* map(char *array, int array_length, char (*f) (char));
+
 char my_get(char c); 
 /* Ignores c, reads and returns a character from stdin using fgetc. */ 
 
@@ -19,6 +21,16 @@ char decrypt(char c);
 
 char xprt(char c); 
 /* xprt prints the value of c in a hexadecimal representation followed by a new line, and returns c unchanged. */ 
+
+
+char* map(char *array, int array_length, char (*f) (char)) {
+    char *res = (char *)(malloc(array_length));
+    for (int i = 0; i < array_length; i++) {
+        res[i] = f(array[i]);
+    }
+    return res;
+}
+
 
 char my_get(char c) {
     c = fgetc(stdin);
@@ -47,10 +59,23 @@ char xprt(char c) {
 }
 
 int main () {
+    char arr1[BUFFSIZE]; 
+    char* arr2 = map(arr1, BUFFSIZE, my_get); 
+    char* arr3 = map(arr2, BUFFSIZE, cprt); 
+    char* arr4 = map(arr3, BUFFSIZE, xprt); 
+    char* arr5 = map(arr4, BUFFSIZE, encrypt); 
+    char* arr6 = map(arr5, BUFFSIZE, decrypt); 
+    /*
     printf("%c\n", my_get('f'));
     cprt('f');
     printf("%c\n", encrypt('f'));
     printf("%c\n", decrypt('f'));
     printf("%c\n", xprt('f'));
+    */
+    free(arr2); 
+    free(arr3); 
+    free(arr4); 
+    free(arr5); 
+    free(arr6); 
     return 0;
 }
