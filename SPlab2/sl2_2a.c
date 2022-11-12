472a192d3b8adc63c3e9c1b283e721edcddacc77
poslevkusie <poslevkusie@MacBook-Pro-Poslevkusie.local>
2022-11-12 12:54:36 +0200

    Splab 2 upd

--- SPlab2/sl2_2a.c
+++ SPlab2/sl2_2a.c
@@ -9,8 +9,17 @@ char* map(char *array, int array_length, char (*f) (char)) {
     return res;
 }
 
+char cens(char c) {
+    if (c == '!') {
+        c = '.';
+    }
+    return c;
+}
+
 int main () {
-    map()
-    free();
+    char arr1[] = {'H','E','Y','!'}; 
+    char* arr2 = map(arr1, 4, cens); 
+    printf("%s\n", arr2); 
+    free(arr2); 
     return 0;
 }
